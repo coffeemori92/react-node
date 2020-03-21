fad7ff07f42114d6ce56ad55f3fd73bbde56bf7f
coffeemori92 <coffeemori92@naver.com>
2020-03-21 16:49:01 +0900

    react 공부중

---
+++ 02_WordRelay/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    name: 'word-relay-setting',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    
+    entry: { // 입력
+        app: ['./client']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR']
+                        },
+                        debug: true
+                }],
+                 '@babel/preset-react'
+                ],
+                plugins: ['@babel/plugin-proposal-class-properties', 'react-hot-loader/babel']
+            }
+        }]
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({ debug: true })
+    ],
+    output: { // 출력
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/dist' // 가상경로
+    }
+};
